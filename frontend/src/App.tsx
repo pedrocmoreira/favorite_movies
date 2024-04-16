5c2fb3432c30c237e70643ca644173cfe52babee
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-04-15 21:26:21 -0300

    chore: iniciando design com shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { router } from './pages/routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
 
   return (
-   <RouterProvider router={router}/>
+    <HelmetProvider>
+      <ThemeProvider storageKey='favoritemovies-theme' defaultTheme='dark'>
+        <Helmet titleTemplate='%s | Favorite Movies' />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
---
+++ frontend/src/pages/_layouts/app.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+export function AppLayout(){
+  return (
+    <div>
+      <Header/>
+
+      <div>
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
